f1c6fee8bc954d1d655cd43c6250bf8effd94620
sethvincent <sethvincent@gmail.com>
2016-07-06 14:29:22 -0700

    huh

---
+++ README.md
@@ -0,0 +1,2 @@
+# experimenting with choo & leaflet
+
---
+++ index.js
@@ -0,0 +1,49 @@
+var leaflet = require('leaflet')
+var html = require('choo/html')
+var choo = require('choo')
+var app = choo()
+
+function mapView (state, prev, send) {
+  function onload (node) {
+    map.invalidateSize()
+    send('map:created')
+  }
+
+  var el = html`<div onload=${onload} id="map"></div>`  
+  var map = createMap(el, state.map)
+  return html`<div class="map-wrapper">${el}</div>`
+}
+
+function createMap (el, options) {
+  var map = L.map(el, options.tiles, options)
+  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    accessToken: 'pk.eyJ1Ijoic2V0aHZpbmNlbnQiLCJhIjoiSXZZXzZnUSJ9.Nr_zKa-4Ztcmc1Ypl0k5nw',
+    id: 'mapbox.streets'
+  }).addTo(map)
+  return map
+}
+
+app.model({
+  namespace: 'map',
+  state: {
+    setView: true,
+    center: [47.606,-122.332],
+    zoom: 11,
+    created: false
+  },
+  reducers: {
+    created: function (data, state) {
+      return { created: true }
+    }
+  }
+})
+
+app.router(function (route) {
+  return [
+    route('/', mapView)
+  ]
+})
+
+var tree = app.start()
+document.body.appendChild(tree)
